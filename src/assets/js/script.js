0a7b46ecf1c8b10e8bd207e7ef901d0d068546a7
Savenok Marta <magnolia.sav@gmail.com>
2023-12-03 00:42:54 +0300

    end

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -180,6 +180,41 @@ if (vacanciesSwiper) {
         }
     });
 }
+let plusSwiper = document.querySelector('.plus-swiper');
+if (plusSwiper) {
+    let swiper = new Swiper(plusSwiper, {
+        pagination: {
+          el: ".swiper-pagination",
+          type: "progressbar",
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            520: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            630: {
+                slidesPerView: 2.5,
+                spaceBetween: 20,
+            },
+            960: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            1024: {
+                slidesPerView: 5,
+                spaceBetween: 30,
+            }
+        }
+      });
+}
 //contact-form
 
 const displayFormBtn = document.querySelector('.display-form-btn');
